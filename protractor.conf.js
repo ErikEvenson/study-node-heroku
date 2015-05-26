2da2cb6de1b1376149c8702309d6e79d8f96c813
Erik Evenson <erik.e.evenson@gmail.com>
2015-05-26 17:38:43 -0500

    Adds end-to-end testing for #1

---
+++ protractor.conf.js
@@ -0,0 +1,29 @@
+exports.config = {
+  baseUrl: 'http://localhost:3001',
+  beforeLaunch: function() {
+    console.log("TBD delete test output.")
+  },
+  directConnect: true,
+  multiCapabilities: [
+    {
+      browserName: 'chrome'
+    }
+    // {
+    //   browserName: 'phantomjs',
+    //   'phantom.binary.path': '/usr/local/node/node-default/bin/phantomjs'
+    // }
+  ],
+  onPrepare: function() {
+    // Inspired by
+    // http://ng-learn.org/2014/02/Protractor_Testing_With_Angular_And_Non_Angular_Sites/
+    global.isAngularSite = function(flag){
+        browser.ignoreSynchronization = !flag;
+    };
+
+    process.env.PORT = 3001
+    require('./index')
+  },
+  specs: [
+    'test/e2e/**/*.spec.js'
+  ]
+}
---
+++ test/e2e/first.spec.js
@@ -0,0 +1,25 @@
+// at the top of the test spec:
+var fs = require('fs');
+
+// abstract writing screen shot to a file
+function writeScreenShot(data, filename) {
+  var stream = fs.createWriteStream(filename);
+
+  stream.write(new Buffer(data, 'base64'));
+  stream.end();
+}
+
+describe('first', function () {
+  beforeEach(function() {
+      isAngularSite(false);
+  });
+
+  it('runs dummy test', function () {
+    browser.ignoreSynchronization = true;
+    browser.get();
+
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'test/output/first.png');
+    });
+  })
+})
