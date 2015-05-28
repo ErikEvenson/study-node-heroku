0a20768ac0a0cb6adf1d3df71aaa42ef908ff976
Erik Evenson <erik.e.evenson@gmail.com>
2015-05-27 19:30:57 -0500

    refactors public

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ var app = express();
 var message = require('./message');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.get('/', function(request, response) {
   response.send(['<p id="hello">', message, '</p>'].join(''));
