591cbea989c91dbf850d103464070f736c91c6a0
Erik Evenson <erik.e.evenson@gmail.com>
2015-05-27 19:29:14 -0500

    refactors unit tests

--- src/server/message.spec.js
+++ src/server/message.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect
-var message = require('../../src/server/message');
+var message = require('./message');
 
 describe('message', function(){
   it('should be equal to "Hello World"', function(){
