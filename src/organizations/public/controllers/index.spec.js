b9cba30809a2d919deed3b417e34f6330fbbf075
Erik Evenson <erik.e.evenson@gmail.com>
2015-06-12 15:01:53 -0500

    simplifies tests

--- src/organizations/public/controllers/index.spec.js
+++ src/organizations/public/controllers/index.spec.js
@@ -2,6 +2,9 @@ require('../module');
 
 describe('Testing Organizations Controller', function() {
   var _scope, OrganizationsController;
+  var Organizations;
+  var $httpBackend;
+  var $routeParams;
 
   beforeEach(function() {
     angular.mock.module('organizations');
@@ -25,49 +28,49 @@ describe('Testing Organizations Controller', function() {
         $scope: _scope
       });
     });
+
+    inject(function(_Organizations_, _$httpBackend_, _$routeParams_) {
+      Organizations = _Organizations_;
+      $httpBackend = _$httpBackend_;
+      $routeParams = _$routeParams_
+    });
   });
 
   it(
     'Should have a find method that uses $resource to retrieve a list of ' +
-    'organizations',
-    inject(function(Organizations) {
-      inject(function($httpBackend) {
-        var sampleOrganization = new Organizations({
-          name: 'organization',
-          email: 'org@example.com'
-        });
-
-        var sampleOrganizations = [sampleOrganization];
-
-        $httpBackend.expectGET('api/organizations')
-          .respond(sampleOrganizations);
-
-        _scope.find();
-        $httpBackend.flush();
-        expect(_scope.organizations).toEqualData(sampleOrganizations);
+    'organizations', function() {
+      var sampleOrganization = new Organizations({
+        name: 'organization',
+        email: 'org@example.com'
       });
-    })
+
+      var sampleOrganizations = [sampleOrganization];
+
+      $httpBackend.expectGET('api/organizations')
+        .respond(sampleOrganizations);
+
+      _scope.find();
+      $httpBackend.flush();
+      expect(_scope.organizations).toEqualData(sampleOrganizations);
+    }
   );
 
   it(
     'Should have a findOne method that uses $resource to retreive a single ' +
-    'of organization',
-    inject(function(Organizations) {
-      inject(function($httpBackend, $routeParams) {
-        var sampleOrganization = new Organizations({
-          name: 'organization',
-          email: 'org@example.com'
-        });
-
-        $routeParams.organizationId = 'abcdef123456789012345678';
-
-        $httpBackend.expectGET(/api\/organizations\/([0-9a-fA-F]{24})$/)
-          .respond(sampleOrganization);
-
-        _scope.findOne();
-        $httpBackend.flush();
-        expect(_scope.organization).toEqualData(sampleOrganization);
+    'of organization', function() {
+      var sampleOrganization = new Organizations({
+        name: 'organization',
+        email: 'org@example.com'
       });
-   })
+
+      $routeParams.organizationId = 'abcdef123456789012345678';
+
+      $httpBackend.expectGET(/api\/organizations\/([0-9a-fA-F]{24})$/)
+        .respond(sampleOrganization);
+
+      _scope.findOne();
+      $httpBackend.flush();
+      expect(_scope.organization).toEqualData(sampleOrganization);
+    }
   );
 });
